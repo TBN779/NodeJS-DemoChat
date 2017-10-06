e43e6eb5130d71d9b2f891db3743b8b3f69cc94e
unknown <tung@coutureconsulting.com>
2017-10-07 02:33:05 +0700

    Login, logout, chatting

--- public/functions.js
+++ public/functions.js
@@ -1,3 +1,57 @@
+    var socket = io("http://localhost:3000")
+
+socket.on("server-send-registration-failed", function(){
+    alert("This username already exist. Please choose another one!");
+});
+
+socket.on("server-send-registraction-successful", function(user){
+    $("#currentUser").html(user);
+    $("#loginForm").hide(2000);
+    $("#chatForm").show(1000);
+});
+
+socket.on("server-send-list-users", function(usersArray){
+    $("#boxContent").html("");
+    usersArray.forEach(function(element) {
+        $("#boxContent").append("<div class='user'>" + element + "</div");
+    });
+});
+
+socket.on("server-send-messages", function(data){
+    $("#listMessages").append("<div class='message'>" + data.user + ": " + data.content + "</div>");
+});
+
+socket.on("someone-is-typing", function(notificationMsg){
+    $("#notification").html(notificationMsg);
+});
+
+socket.on("someone-loss-of-typing", function(notificationMsg){
+    // $("#notification").html("");
+});
+
 $(document).ready(function(){
-    alert("1");
+    $("#loginForm").show();
+    $("#chatForm").hide();
+
+    $("#btnRegister").click(function(){
+        socket.emit("client-send-username", $("#txtUername").val());
+    });
+
+    $("#btnLogout").click(function(){
+        socket.emit("logout");
+        $("#loginForm").show(1000);
+        $("#chatForm").hide(2000);
+    });
+
+    $("#btnSendMessage").click(function() {
+        socket.emit("user-send-message", $("#txtMessage").val());
+    })
+
+    $("#txtMessage").focusin(function(){
+        socket.emit("typing");
+    });
+
+    $("#txtMessage").focusin(function(){
+        socket.emit("loss-of-focus");
+    });
 });
--- server.js
+++ server.js
@@ -10,9 +10,43 @@ server.listen(3000, function () {
 	console.log("Server listening on port 3000.");
 });
 
-// app.get("/", function(req, res){
-//     res.render("home");
-// });
-app.get("/", function(request, response) {
-    response.send("Hello, world!");
-}); 
+
+var usersArray=["tung"];
+
+io.on("connection", function(socket){
+    console.log("Incoming connection " + socket.id);
+
+    socket.on("client-send-username", function(user){
+        if(usersArray.indexOf(user) >=0) { //registration failed            
+            socket.emit("server-send-registration-failed");            
+        } else { //registration successful    
+            usersArray.push(user);
+            socket.username = user; //create a new property named username
+            socket.emit("server-send-registraction-successful", user);
+            io.sockets.emit("server-send-list-users", usersArray);
+        }
+    });
+
+    socket.on("logout", function(){
+        usersArray.splice(
+            usersArray.indexOf(socket.username), 1
+        );
+        socket.broadcast.emit("server-send-list-users", usersArray);
+    });
+
+    socket.on("user-send-message", function(message){
+        io.sockets.emit("server-send-messages", {user:socket.username, content:message});
+    });
+
+    socket.on("typing", function(){
+        var notificationMsg = socket.username + " is typing...";
+        io.sockets.emit("someone-is-typing", notificationMsg);
+    });
+    socket.on("loss-of-focus", function(){
+        // io.sockets.emit("someone-loss-of-typing");
+    });
+});
+
+app.get("/", function(req, res){
+    res.render("home");
+});
